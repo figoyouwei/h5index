4937b0af495c491f5cff54cba4654085065d2575
figoyouwei <figoyouwei@gmail.com>
2017-01-25 16:07:26 +0800

    upload h5009

---
+++ h5009/js/script.js
@@ -0,0 +1,121 @@
+if(typeof $contextPath == 'undefined'){
+	var $contextPath = "";
+}else{
+	$contextPath += "/template/h5009/code/";
+}
+function include(url){
+  document.write('<script src="'+$contextPath+url+'"></script>');
+  return false ;
+}
+
+/* Stick up menu
+========================================================*/
+include('js/tmstickup.js');
+$(window).load(function() { 
+  if ($('html').hasClass('desktop')) {
+      $('#stuck_container').TMStickUp({})
+  }  
+});
+
+/* Superfish menu
+========================================================*/
+include('js/superfish.js');
+include('js/jquery.mobilemenu.js');
+
+/* Stellar.js
+========================================================*/
+include('js/jquery.stellar.js');
+$(document).ready(function() { 
+  if ($('html').hasClass('desktop')) {
+      $.stellar({
+        horizontalScrolling: false,
+        verticalOffset: -2000
+      });
+  }  
+});
+
+/* DEVICE.JS
+========================================================*/
+include('js/device.min.js');
+
+/* Easing library
+========================================================*/
+include('js/jquery.easing.1.3.js');
+
+/* Equal Heights
+========================================================*/
+include('js/jquery.equalheights.js');
+
+/* DEVICE.JS AND SMOOTH SCROLLIG
+========================================================*/
+include('js/jquery.mousewheel.min.js');
+include('js/jquery.simplr.smoothscroll.min.js');
+$(function () { 
+  if ($('html').hasClass('desktop')) {
+      $.srSmoothscroll({
+        step:150,
+        speed:800
+      });
+  }   
+});
+
+/* Orientation tablet fix
+========================================================*/
+$(function(){
+// IPad/IPhone
+	var viewportmeta = document.querySelector && document.querySelector('meta[name="viewport"]'),
+	ua = navigator.userAgent,
+
+	gestureStart = function () {viewportmeta.content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6, initial-scale=1.0";},
+
+	scaleFix = function () {
+		if (viewportmeta && /iPhone|iPad/.test(ua) && !/Opera Mini/.test(ua)) {
+			viewportmeta.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+			document.addEventListener("gesturestart", gestureStart, false);
+		}
+	};
+	
+	scaleFix();
+	// Menu Android
+	if(window.orientation!=undefined){
+  var regM = /ipod|ipad|iphone/gi,
+   result = ua.match(regM)
+  if(!result) {
+   $('.sf-menu li').each(function(){
+    if($(">ul", this)[0]){
+     $(">a", this).toggle(
+      function(){
+       return false;
+      },
+      function(){
+       window.location.href = $(this).attr("href");
+      }
+     );
+    } 
+   })
+  }
+ }
+});
+var ua=navigator.userAgent.toLocaleLowerCase(),
+ regV = /ipod|ipad|iphone/gi,
+ result = ua.match(regV),
+ userScale="";
+if(!result){
+ userScale=",user-scalable=0"
+}
+document.write('<meta name="viewport" content="width=device-width,initial-scale=1.0'+userScale+'">')
+
+/* Copyright Year
+========================================================*/
+var currentYear = (new Date).getFullYear();
+$(document).ready(function() {
+  $("#copyright-year").text( (new Date).getFullYear() );
+});
+
+/* ToTop
+========================================================*/
+include('js/jquery.ui.totop.js');
+$(function () {   
+  $().UItoTop({ easingType: 'easeOutQuart' });
+});
+
