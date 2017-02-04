dbb3165447f8d2262b37a1b83eed983babb26f93
Youwei Zheng <figoyouwei@gmail.com>
2017-02-04 11:00:19 +0800

    upload h5016 - h5024

---
+++ h5021/js/script.js
@@ -0,0 +1,157 @@
+var varjs = "";
+var templateId = "5021";
+if(typeof $contextPath == 'undefined'){
+	$contextPath = varjs;
+}else{
+	$contextPath = $contextPath+"/template/h"+templateId+"/code/"+varjs;
+}
+function include(url){
+  document.write('<script src="'+$contextPath+url+'"></script>');
+  return false ;
+}
+
+/* cookie.JS
+ ========================================================*/
+include('js/jquery.cookie.js');
+
+
+/* DEVICE.JS
+ ========================================================*/
+include('js/device.min.js');
+
+/* Stick up menu
+ ========================================================*/
+include('js/tmstickup.js');
+$(window).load(function () {
+    if ($('html').hasClass('desktop')) {
+        $('#stuck_container').TMStickUp({
+        })
+    }
+});
+
+
+/* Easing library
+ ========================================================*/
+include('js/jquery.easing.1.3.js');
+
+/* ToTop
+ ========================================================*/
+include('js/jquery.ui.totop.js');
+$(function () {
+    $().UItoTop({ easingType: 'easeOutQuart' });
+});
+
+
+/* DEVICE.JS AND SMOOTH SCROLLIG
+ ========================================================*/
+include('js/jquery.mousewheel.min.js');
+include('js/jquery.simplr.smoothscroll.min.js');
+$(function () {
+    if ($('html').hasClass('desktop')) {
+        $.srSmoothscroll({
+            step: 150,
+            speed: 800
+        });
+    }
+});
+
+/* Copyright Year
+ ========================================================*/
+var currentYear = (new Date).getFullYear();
+$(document).ready(function () {
+    $("#copyright-year").text((new Date).getFullYear());
+});
+
+
+/* Superfish menu
+ ========================================================*/
+include('js/superfish.js');
+include('js/jquery.mobilemenu.js');
+
+/* Unveil
+ ========================================================*/
+include('js/jquery.unveil.js');
+$(document).ready(function () {
+    $('img').unveil(0, function () {
+        $(this).load(function () {
+            $(this).addClass("js-lazy-img");
+        });
+    });
+});
+
+/* Orientation tablet fix
+ ========================================================*/
+$(function () {
+// IPad/IPhone
+    var viewportmeta = document.querySelector && document.querySelector('meta[name="viewport"]'),
+        ua = navigator.userAgent,
+
+        gestureStart = function () {
+            viewportmeta.content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6, initial-scale=1.0";
+        },
+
+        scaleFix = function () {
+            if (viewportmeta && /iPhone|iPad/.test(ua) && !/Opera Mini/.test(ua)) {
+                viewportmeta.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+                document.addEventListener("gesturestart", gestureStart, false);
+            }
+        };
+
+    scaleFix();
+    // Menu Android
+    if (window.orientation != undefined) {
+        var regM = /ipod|ipad|iphone/gi,
+            result = ua.match(regM);
+        if (!result) {
+            $('.sf-menu li').each(function () {
+                if ($(">ul", this)[0]) {
+                    $(">a", this).toggle(
+                        function () {
+                            return false;
+                        },
+                        function () {
+                            window.location.href = $(this).attr("href");
+                        }
+                    );
+                }
+            })
+        }
+    }
+});
+var ua = navigator.userAgent.toLocaleLowerCase(),
+    regV = /ipod|ipad|iphone/gi,
+    result = ua.match(regV),
+    userScale = "";
+if (!result) {
+    userScale = ",user-scalable=0"
+}
+document.write('<meta name="viewport" content="width=device-width,initial-scale=1.0' + userScale + '">')
+
+$(document).ready(function () {
+    var obj;
+
+    if ((obj = $('#camera')).length > 0) {
+        obj.camera({
+            height: '49.583%',
+            minHeight: '200px',
+            pagination: true,
+            thumbnails: false,
+            playPause: false,
+            hover: false,
+            loader: 'none',
+            navigation: false,
+            navigationHover: false,
+            mobileNavHover: false,
+            fx: 'simpleFade'
+        })
+    }
+
+    if ((obj = $('a[data-type="lightbox"]')).length > 0) {
+        obj.touchTouch();
+    }
+
+    if ((obj = $('#tabs')).length > 0) {
+        obj.easyResponsiveTabs();
+    }
+});
+
